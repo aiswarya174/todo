fb506d74877737c0797389eb802888ed7e57a486
aiswarya174 <aiswaryamadhu30@gmail.com>
2026-02-12 11:27:06 +0530

    todo

---
+++ src/ToDo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ToDo() {
+  return (
+    <div>
+         <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default ToDo
